79a736d55564be479fdd818cdcaf3d3f85dbf503
Marco Reimann <marcoreimann@outlook.de>
2018-09-06 09:02:48 +0200

    Update github.php

--- src/routes/github.php
+++ src/routes/github.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::post(config('github.uri'), config('github.controller'));
+Route::post(config('github.uri'), config('github.controller'))->middleware(config('github.middleware-name'));
